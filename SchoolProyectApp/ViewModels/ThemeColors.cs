496d51027a043925cdf9f9141f310476dcdafe3d
randy4242 <randygodoy09@gmail.com>
2025-09-02 11:20:45 -0400

    Resolucion de error con colores remanentes

--- SchoolProyectApp/ViewModels/ThemeColors.cs
+++ SchoolProyectApp/ViewModels/ThemeColors.cs
@@ -6,12 +6,12 @@ namespace SchoolProyectApp.ViewModels
 {
     public static class ThemeColors
     {
-        public static Color PrimaryColor { get; } = Color.FromArgb("#0d4483");
+        public static Color PrimaryColor { get; } = Color.FromArgb("#0d4483"); //"{x:Static vm:ThemeColors.PrimaryColor}"
         public static Color SecondaryColor { get; } = Color.FromArgb("#0098da");
         public static Color AccentColor { get; } = Color.FromArgb("#e99a27");
         public static Color PageBackgroundColor { get; } = Color.FromArgb("#FFFFFF");
 
-        public static Color TextColor { get; } = Color.FromArgb("#FFFFFF");
+        public static Color TextColor { get; } = Color.FromArgb("#0d4483");
 
         public static Color DangerColor { get; } = Color.FromArgb("#FF0000");
     }
